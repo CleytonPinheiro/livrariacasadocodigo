d92bf59c18c55b3197983fd19859731d280ff8e7
CleytonPinheiro <cleyton_pin@hotmail.com>
2020-06-15 16:15:02 -0300

    Remoção, edição de livros, reescritas métodos HTTP e arquivos estáticos

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -1,4 +1,4 @@
-class LivroDao{
+class LivroDao {
 
     constructor(db) {
         this._db = db;
@@ -7,12 +7,12 @@ class LivroDao{
     adiciona(livro) {
         return new Promise((resolve, reject) => {
             this._db.run(`
-            INSERT INTO LIVROS (
+                INSERT INTO livros (
                     titulo,
                     preco,
                     descricao
-                ) values (?, ?, ?)
-            `,
+                ) values (?,?,?)
+                `,
                 [
                     livro.titulo,
                     livro.preco,
@@ -23,29 +23,89 @@ class LivroDao{
                         console.log(err);
                         return reject('Não foi possível adicionar o livro!');
                     }
+
                     resolve();
                 }
             )
         });
     }
 
-    lista(){
+    lista() {
         return new Promise((resolve, reject) => {
             this._db.all(
                 'SELECT * FROM livros',
                 (erro, resultados) => {
-                    if (erro) return reject ('Não foi possível listar os livros!');
+                    if (erro) return reject('Não foi possível listar os livros!');
 
                     return resolve(resultados);
                 }
             )
-        })
+        });
+    }
 
-        this._db.all(
-            'SELECT * FROM livros',
-            (erro, resultados) =>
-                callback(erro, resultados)
-        )
+    buscaPorId(id) {
+
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    SELECT *
+                    FROM livros
+                    WHERE id = ?
+                `,
+                [id],
+                (erro, livro) => {
+                    if (erro) {
+                        return reject('Não foi possível encontrar o livro!');
+                    }
+                    return resolve(livro);
+                }
+            );
+        });
+    }
+
+    atualiza(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                UPDATE livros SET
+                titulo = ?,
+                preco = ?,
+                descricao = ?
+                WHERE id = ?
+            `,
+            [
+                livro.titulo,
+                livro.preco,
+                livro.descricao,
+                livro.id
+            ],
+            erro => {
+                if (erro) {
+                    return reject('Não foi possível atualizar o livro!');
+                }
+
+                resolve();
+            });
+        });
+    }
+
+    remove(id) {
+
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    DELETE
+                    FROM livros
+                    WHERE id = ?
+                `,
+                [id],
+                (erro) => {
+                    if (erro) {
+                        return reject('Não foi possível remover o livro!');
+                    }
+                    return resolve();
+                }
+            );
+        });
     }
 }
 
